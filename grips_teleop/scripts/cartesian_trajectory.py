01a686655ff55f46e54b270b97e326a3af419990
fsuarez6 <fsuarez6@gmail.com>
2015-02-20 16:23:39 +0100

    Added F/T sensor model to gazebo (not the plugin) for controlling the robot in the sensor reference frame

--- grips_teleop/scripts/cartesian_trajectory.py
+++ grips_teleop/scripts/cartesian_trajectory.py
@@ -10,19 +10,20 @@ from geometry_msgs.msg import PoseStamped
 class CartersianTrajectory(object):
   def __init__(self):
     # Parse the yaml file to obtain the trajectory
-    time_step = read_parameter('~time_step', 0.05)
+    time_step = float(read_parameter('~time_step', 0.05))
     points = read_parameter('~points', list())
     frame_id = read_parameter('~frame_id', 'base_link')
-    rate = rospy.Rate(1 / time_step)
     # Set-up publishers/subscribers
     cmd_pub = rospy.Publisher('/grips/ik_command', PoseStamped)
+    # Give it time to create the publisher
+    rospy.sleep(2.0)
     for point in points:
       pose = PoseConv.to_pose_stamped_msg(frame_id, point[4:7], point[:4])
       cmd_pub.publish(pose)
       try:
-        rate.sleep()
+        rospy.sleep(time_step)
       except:
-        break
+        pass
 
 if __name__ == '__main__':
   node_name = os.path.splitext(os.path.basename(__file__))[0]
