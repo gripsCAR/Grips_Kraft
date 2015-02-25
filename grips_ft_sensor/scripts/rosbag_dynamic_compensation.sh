67675133ca5c39ce738faa14d1bceb704151f0fe
fsuarez6 <fsuarez6@gmail.com>
2015-02-25 18:42:15 +0100

    Fixed robotiq center of mass [ci skip]

---
+++ grips_ft_sensor/scripts/rosbag_dynamic_compensation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rosbag record /netft/data /netft/filtered /netft/compensated /grips/ik_command /grips/endpoint_state /grips/endpoint_acc
--- grips_teleop/scripts/dynamic_compensation_record.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rosbag record /netft/data /netft/filtered /netft/compensated /grips/endpoint_state /grips/ik_command
